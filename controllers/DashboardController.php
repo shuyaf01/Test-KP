d0249c76c36f560891490948823780bb9c3c8d75
shuyaf01 <45596718+shuyaf01@users.noreply.github.com>
2021-09-06 20:05:50 +0700

    Update DashboardController.php

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DashboardController extends CI_Controller {
+class DashboardController extends CI_Controller {   
     /**
      * DashboardController constructor.
      */
